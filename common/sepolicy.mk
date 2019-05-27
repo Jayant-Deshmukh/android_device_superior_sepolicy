fbc403a3ce3c9b47e571e498c88dfdd59f4a2b41
Jayant Deshmukh <jayantdeshmuk008@gmail.com>
2019-05-27 03:33:03 +0000

    sepolicy: Changes for SuperiorOS

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Havoc
+# inherit from superior
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/havoc/sepolicy/common/public
+    device/superior/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/havoc/sepolicy/common/private
+    device/superior/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/havoc/sepolicy/common/vendor
+    device/superior/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Havoc
+# that inherit from superior
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/havoc/sepolicy/qcom/private
+    device/superior/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/havoc/sepolicy/qcom/common \
-    device/havoc/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/superior/sepolicy/qcom/common \
+    device/superior/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
